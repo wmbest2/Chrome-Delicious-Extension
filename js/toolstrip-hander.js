fc2c4d5d4466386d42614d6b86ea22f5f7b95b78
Scott Ferguson <sferguson@quikorder.com>
2009-09-02 16:45:04 -0500

    More code cleanup and start page actions API

--- js/toolstrip-hander.js
+++ js/toolstrip-hander.js
@@ -7,9 +7,20 @@ chrome.self.onConnect.addListener(function(port) {
     port.onMessage.addListener(function(data) {
         url = data.url;
         title = data.title;
+
+        // Register the tab with the tagging page action
+        chrome.pageActions.enableForTab("tag_page",
+                                        { 
+                                            tabId: port.tab.id,
+                                            url: port.tab.url
+                                        });
     });
 });
 
-function add_delicious() {
+chrome.pageActions["tag_page"].addListener(function(actionId, reply) {
+    tagCurrentPage();
+});
+
+function tagCurrentPage() {
     window.open(deliciousUrl + url + '&title=' + title +' ','deliciousuiv5','location=yes,links=no,scrollbars=no,toolbar=no,width=550,height=550');
 };
